836b3832dec3e6e5c47b7a519e0d5dcbe99a03b4
Carlos Palomino <palominocp@outlook.com>
2020-10-12 23:53:03 -0500

    D3 Scatter

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,58 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 30,
+    left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+    .select('#scatter')
+    .append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("/assets/data/data.csv").then((data) => {
+
+    data.forEach(d => {
+        d.age = +d.age;
+        d.smokes = +d.smokes;
+    });
+
+    var xLinearScale = d3.scaleLinear().range([0, width])
+        .domain(d3.extent(data, d => d.age));
+
+    var yLinearScale = d3.scaleLinear().range([height, 0])
+        .domain(d3.extent(data, d => d.smokes));
+        
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale).ticks(6);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    chartGroup.append("g")
+        .call(yAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", d => xLinearScale(d.age))
+            .attr("cy", d => yLinearScale(d.smokes))
+            .attr("r", "10")
+            .attr("fill", "gold")
+            .attr("stroke-width", "1")
+            .attr("stroke", "black");
+
+y});
