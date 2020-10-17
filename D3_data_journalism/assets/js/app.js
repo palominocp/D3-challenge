2c1bbca02edc4d79db784ee116afe268f066caf6
Carlos Palomino <palominocp@outlook.com>
2020-10-17 15:49:52 -0500

    Circle text fixed

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -21,6 +21,8 @@ var svg = d3
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+var yLabel = "Lacks Healthcare (%)";
+
 d3.csv("/assets/data/data.csv").then((data) => {
 
     data.forEach(d => {
@@ -53,24 +55,44 @@ d3.csv("/assets/data/data.csv").then((data) => {
             .attr("cx", d => xLinearScale(d.poverty))
             .attr("cy", d => yLinearScale(d.healthcare))
             .attr("r", "10")
-            .attr("fill", "blue")
+            .attr("fill", "rgb(136, 187, 213)")
             .style("opacity", 0.4)
             //.attr("stroke-width", "0")
             //.attr("stroke", "black")
     
-    var elemEnter = chartGroup.selectAll("g")
+    //var elemEnter = chartGroup.selectAll("g")
+      //  .data(data)
+        //.enter()
+        //.append("g")
+        //.attr("transform", d => `translate(${xLinearScale(d.poverty)} , ${yLinearScale(d.healthcare)})`)
+
+        chartGroup.selectAll("circles")
         .data(data)
         .enter()
         .append("g")
         .attr("transform", d => `translate(${xLinearScale(d.poverty)} , ${yLinearScale(d.healthcare)})`)
-
-    elemEnter.append("text")
+        .append("text")
         .attr("dx", function(d){return -8})
         .attr("dy", function(d){return +4})
         .attr("fill", "white")
         .text(function(d) {
             console.log(d.abbr);
-            return d.state})
+            return d.abbr})
         .style("font-size", "10px");
+
+        // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", - 60) // margin.left + 40
+    .attr("x", - 260) // (height / 2)
+    .attr("dy", "1em")
+    //.attr("class", "axisText")
+    .text(yLabel);
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2 - 100}, ${height + margin.top + 5})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
+
     
 });
